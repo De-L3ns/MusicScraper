fad6c8a9efe27102b6b11ac1580d4ccda03eea12
Laurens <70441547+De-L3ns@users.noreply.github.com>
2022-01-03 17:00:10 +0100

    changes for heroku deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 import fetch from 'node-fetch'; // gets the node-fetch package
 import { load } from 'cheerio'; // gets the cheerio package
 
