c979f762782994b776f19cd605efe7cd7f9cf582
Laurens <70441547+De-L3ns@users.noreply.github.com>
2022-01-20 21:36:12 +0100

    added weeks in a row table row and data

--- app.js
+++ app.js
@@ -10,24 +10,9 @@ import moment from 'moment';
 // Makes __dirname work
 import path from 'path';
 import res from 'express/lib/response.js';
+import { Console } from 'console';
 
 const __dirname = path.resolve(path.dirname(decodeURI(new URL(import.meta.url).pathname)));
-
-
-// Static files
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
-
-// Views for EJS
-app.set('views', './views');
-app.set('view engine', 'ejs');
-
-
-// Listen on port 3000
-
-app.listen(port, () => console.info(`listening on port ${port}`));
 
 
 // # Functions
@@ -97,11 +82,13 @@ const getTop50Artists = async (url) => {
     
 
     parsedTop50Data('b',".chart_title").each(function (i, e) {
-        artistList[i] = parsedTop50Data(this).text();
+        
+        artistList[i] = parsedTop50Data(this).text().replace('�', 'e');
+        // .replace get's rid of the é ë and è problem.
         
     });
     
-    
+   
     return artistList;
     
 };
@@ -114,21 +101,49 @@ const getTop50Songs = async (url) => {
     parsedTop50Data('a', ".chart_title").each(function (i, e) {
 
         parsedTop50Data(this).contents().map(function(){
-            songList[i] = (this.type === 'text') ? parsedTop50Data(this).text()+'' : '';
+            songList[i] = (this.type === 'text') ? parsedTop50Data(this).text().replace('�', 'e') +'' : '';
         }).get().join('');
-        
+        // .replace get's rid of the é ë and è problem.
     })
 
     return songList;
     
 }; 
 
+const getWeeksInARow = async (url) => {
+    const top50Data = await getRawData(url);
+    const parsedTop50Data = load(top50Data, { decodeEntities: false });
+    let weeks = [];
+    
+    parsedTop50Data('.wp', ".chart_addinfo").each(function (i, e) {
+
+        weeks[i] = parsedTop50Data(this).text().split('|')[0].substring(1).trim();
+        
+    });
+
+
+    return weeks;
+    
+}; 
+
 let top50ArtistsThisWeek = await getTop50Artists(url);
 let top50SongsThisWeek = await getTop50Songs(url);
+let getWeeks = await getWeeksInARow(url);
 let top50ArtistsLastWeek = await getTop50Artists(urlPrevWeek);
 let top50SongsLastWeek = await getTop50Songs(urlPrevWeek);
 
-// Render the scraped data to EJS.
+
+// Static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+// Views for EJS
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
 
 app.get('', (req, res) => {
 
@@ -140,8 +155,13 @@ app.get('', (req, res) => {
             top50SongsThisWeek: top50SongsThisWeek,
             top50ArtistsLastWeek: top50ArtistsLastWeek,
             top50SongsLastWeek: top50SongsLastWeek,
+            getWeeks: getWeeks,
         })
         
     
     
 });
+
+// Listen on port 3000
+
+app.listen(port, () => console.info(`listening on port ${port}`));
